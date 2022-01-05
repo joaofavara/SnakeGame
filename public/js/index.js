002f53c5feefd634055d8401729b50e7752d115f
joaofavara <joaopedro_favara@hotmail.com>
2022-01-05 16:32:49 -0300

    feat: create full move for snake

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,23 @@
 // import { SafeString } from 'hbs';
+const snake = {
+  x: 0,
+  y: 0,
+};
 
 const board = Array(10)
   .fill(null)
   .map(() => Array(10).fill(0));
 
-board[0][0] = 1;
+board[snake.x][snake.y] = 1;
 
 const displayBoard = () => {
-  console.log('displayBoard');
+  const { x, y } = snake;
   let total = '';
   for (let j = 0; j < 10; j++) {
     let data = '<tr>';
 
     for (let i = 0; i < 10; i++) {
-      if (board[j][i] === 1) {
+      if (board[j][i] === 1 && j === y && i === x) {
         data += `<td class="snake"></td>`;
       } else {
         data += `<td></td>`;
@@ -34,16 +38,43 @@ function createBoard() {
 window.onload = function () {
   const video = document.body;
   video.innerHTML = createBoard();
-  console.log('document: ', document);
-  console.log('video: ', video);
 
   video.addEventListener('keydown', (event) => {
     const { key } = event;
-    console.log(key);
-    if (key === 'ArrowRight') {
-      board[1][1] = 1;
-      board[0][0] = 0;
-      video.innerHTML = createBoard();
+
+    switch (key) {
+      case 'ArrowRight':
+        if (snake.x + 1 < 10) {
+          board[snake.y][snake.x] = 0;
+          snake.x += 1;
+          board[snake.y][snake.x] = 1;
+          video.innerHTML = createBoard();
+        }
+        break;
+      case 'ArrowLeft':
+        if (snake.x - 1 >= 0) {
+          board[snake.y][snake.x] = 0;
+          snake.x -= 1;
+          board[snake.y][snake.x] = 1;
+          video.innerHTML = createBoard();
+        }
+        break;
+      case 'ArrowDown':
+        if (snake.y + 1 < 10) {
+          board[snake.y][snake.x] = 0;
+          snake.y += 1;
+          board[snake.y][snake.x] = 1;
+          video.innerHTML = createBoard();
+        }
+        break;
+      case 'ArrowUp':
+        if (snake.y - 1 >= 0) {
+          board[snake.y][snake.x] = 0;
+          snake.y -= 1;
+          board[snake.y][snake.x] = 1;
+          video.innerHTML = createBoard();
+        }
+        break;
     }
   });
 };
